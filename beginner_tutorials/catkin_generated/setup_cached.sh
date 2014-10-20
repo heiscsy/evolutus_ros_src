add1cd0d4b5bc084e4c67426466a9c562da35eca
Yuhan Long <heiscsy@gmail.com>
2014-10-20 01:46:40 -0400

    initial workspace

---
+++ beginner_tutorials/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/yuhan/catkin_ws/src/beginner_tutorials/test_results"
+export CMAKE_PREFIX_PATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/lib:/home/yuhan/catkin_ws/src/beginner_tutorials/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/lib/pkgconfig:/home/yuhan/catkin_ws/src/beginner_tutorials/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yuhan/catkin_ws/src/beginner_tutorials/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/yuhan/catkin_ws/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/yuhan/catkin_ws/src/beginner_tutorials/test_results"
---
+++ beginner_tutorials/gazebo_ros_pkgs/gazebo_msgs/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/yuhan/catkin_ws/src/beginner_tutorials/devel/include".split(';') if "/home/yuhan/catkin_ws/src/beginner_tutorials/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime;std_msgs;trajectory_msgs;geometry_msgs;sensor_msgs;std_srvs;message_generation".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "gazebo_msgs"
+PROJECT_SPACE_DIR = "/home/yuhan/catkin_ws/src/beginner_tutorials/devel"
+PROJECT_VERSION = "2.4.6"
